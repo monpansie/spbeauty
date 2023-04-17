84273ba76abbd4f06e5dc6a5f3a18b575a7b8b75
Elena Sokolova <131026987+monpansie@users.noreply.github.com>
2023-04-17 22:20:01 +0000

    SecondaryButton

--- src/Components/SecondaryButton.js
+++ src/Components/SecondaryButton.js
@@ -5,27 +5,6 @@ function SecondaryButton(props) {
     // Здесь устанавливается минимальная ширина для кнопки
     let minWidth = props.minWidth === undefined ? '144px' : props.minWidth;
     const Button = styled.div`
-        background: transparent;
-        border: 1px solid #DFDFDF;
-        border radius: 5px;
-        box-shadow: inset 0px 1px 2px rgba(10, 15, 26, 0.08);
-        min-width: ${minWidth};
-        color: #152842;
-        &:hover {
-            background: linear-gradient(180deg, #DFEAF1 0%, #CFDDE8 100%);
-        }
-        &:active {
-            background: #DFDFDF;
-        }
-    `;
-    return (
-        <Clickable >
-            <Button className='p-2 text-md'>
-                {props.children}
-            </Button>
-        </Clickable>
-        
-    );
-}
+       
 
 export default SecondaryButton;
